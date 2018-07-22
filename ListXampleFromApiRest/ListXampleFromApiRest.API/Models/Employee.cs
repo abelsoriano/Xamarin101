11f73fcdcfd93c749b6f87c7a4d66f5f70c897c7
Starling Germosen <sgrysoft@outlook.com>
2018-07-22 17:27:54 -0400

    Api creation

---
+++ ListXampleFromApiRest/ListXampleFromApiRest.API/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ListXampleFromApiRest.API.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
